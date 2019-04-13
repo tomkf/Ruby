14ceb14fac5edc60349648833953ebb033654eb8
Thomas Ferris <thomaskeithferris@gmail.com>
2019-04-13 12:11:04 -0400

    simple black jack game

---
+++ black_jack/black_jack.rb
@@ -0,0 +1,13 @@
+def pick_bank_score
+  # TODO: Use Random to get a new random score
+  back_score = rand(16..21)
+  return back_score
+end
+
+def pick_player_card
+  # TODO: Use Random to get a new random card
+    card_value = rand(1..11)
+  return card_value
+end
+
+
---
+++ black_jack/croupier.rb
@@ -0,0 +1,57 @@
+require_relative 'black_jack'
+
+def state_of_the_game(player_score, bank_score)
+  # TODO: return (not print!) a message containing the player's score and bank's score
+
+  return "Your score is #{player_score} bank is #{bank_score}"
+end
+
+
+ def end_game_message(player_score, bank_score)
+   # TODO: return (not print!) a message telling if the user won or lost.
+ win = nil
+   if player_score == bank_score
+    return "Push"
+    return player_score
+  elsif player_score == 21
+    win = true
+    return "Black Jack"
+   elsif player_score > 21
+    win = false
+    return "You lost the game"
+   elsif player_score > bank_score
+ win = true
+ return "You won"
+ return
+ elsif player_score < bank_score
+   win = false
+   return "You lost"
+   end
+ end
+
+
+ def play_game
+  # TODO: make the user play from terminal in a while loop that will stop
+  #       when the user will not be asking for  a new card
+   bank_value =  pick_bank_score()
+  finish_condition = false
+  player_score = 0
+  until finish_condition do
+  puts "Card? 'y' or 'yes' to get a new card"
+ user_input = gets.chomp
+ player_value = pick_player_card()
+ player_score += player_value
+  if user_input == "y" || user_input == "yes"
+   puts state_of_the_game(bank_value, player_score)
+ else
+  finish_condition = true
+  puts end_game_message(player_score, bank_value)
+end
+
+ end
+
+ end
+
+
+
+
