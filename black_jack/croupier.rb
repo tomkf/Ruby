39178759f9b05c0adca95ac046bbd9fa4e1f5367
Thomas Ferris <thomaskeithferris@gmail.com>
2019-04-21 14:48:50 -0400

    Update croupier.rb

--- black_jack/croupier.rb
+++ black_jack/croupier.rb
@@ -1,14 +1,12 @@
 require_relative 'black_jack'
 
 def state_of_the_game(player_score, bank_score)
-  # TODO: return (not print!) a message containing the player's score and bank's score
 
   return "Your score is #{player_score} bank is #{bank_score}"
 end
 
 
  def end_game_message(player_score, bank_score)
-   # TODO: return (not print!) a message telling if the user won or lost.
  win = nil
    if player_score == bank_score
     return "Push"
@@ -31,8 +29,6 @@ end
 
 
  def play_game
-  # TODO: make the user play from terminal in a while loop that will stop
-  #       when the user will not be asking for  a new card
    bank_value =  pick_bank_score()
   finish_condition = false
   player_score = 0
